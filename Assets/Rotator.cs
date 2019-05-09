ef6056b67c3c2d9e1b636bcfe036ad8e5a1505f4
Ian Rapoport <iangrap@gmail.com>
2019-05-08 21:30:25 -0700

    Finished first pass of tron shader

---
+++ Assets/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotateSpeed = 1f;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, Time.deltaTime * rotateSpeed, 0f));
+    }
+}
